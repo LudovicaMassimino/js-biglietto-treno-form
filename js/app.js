02d90185f86c28f0cff2a8e2315069a15b683751
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-13 19:36:32 +0100

    details

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 console.log('form-biglietto del treno')
 
 
-const buttonElement = document.getElementById('submit')
-console.log(buttonElement)
 
+//per prendere le info dagli input di km e age
 const kmInputElement = document.getElementById('km')
 console.log(kmInputElement)
 
 const ageInputElement = document.getElementById('age')
 console.log(ageInputElement)
 
+//button
+const buttonElement = document.getElementById('submit')
+console.log(buttonElement)
 
 //ascolto del click sul button
 buttonElement.addEventListener('click', function () {
     console.log('click sul button')
 
+// km che vuoi percorrere
 const km = parseFloat(kmInputElement.value) ; // number
-console.log('I km che vuoi percorrere: ' + km)
+console.log('I km che vuoi percorrere: ' + km + ' km')
 
+// la tua età
 const age = parseFloat(ageInputElement.value) ; // number
-console.log ('La tua età: ' + age)
+console.log ('La tua età: ' + age + ' anni')
 
 // totale provvisorio
-const prezzoProvvisorio = (0.21 * km); //number
-    console.log ('Totale provvisorio: ' + prezzoProvvisorio + ' euro')  
+const totaleProvvisorio = (0.21 * km); //number
+    console.log ('Totale provvisorio: ' + totaleProvvisorio + ' euro')  
 
 // condizioni per lo sconto: SE <18 20%, SE >65 40%
     let sconto = 0 // number
@@ -37,9 +41,10 @@ const prezzoProvvisorio = (0.21 * km); //number
         console.log ('Non puoi usufruire dello sconto')
     }
 
-    const price = (prezzoProvvisorio - prezzoProvvisorio * sconto);
-    console.log (price.toFixed(2))
+    const price = totaleProvvisorio - totaleProvvisorio * sconto;
+    console.log ('Il tuo biglietto costerà: ' + price.toFixed(2) + ' euro')
     
+    // stampare il prezzo nella viewport
     const priceElement = document.getElementById('price')
     console.log(priceElement)
     priceElement.innerHTML = price.toFixed(2) +  ' &euro;'
